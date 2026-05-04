1c7a21530a4b90f3ad6a2b9d7ba5d527221105e8
ursilapradeep <https://github.com/ursilapradeep>
2026-05-04 17:38:04 +0200

    created_file Account.cpp

---
+++ ex02/Account.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uvadakku <uvadakku@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 17:22:24 by uvadakku          #+#    #+#             */
+/*   Updated: 2026/05/04 17:34:39 by uvadakku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream> 
+#include <iomanip> 
+#include <ctime> 
+#include "Account.hpp" 
+
+int Account::_nbAccounts = 0;
+int Account::_TotalAmount = 0;
+int Account::_	
